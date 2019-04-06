04612835a5c76a93f6ebbf3556f05a3b8e0c4aca
Maciej Hełmecki <mhelmecki@gmail.com>
2019-04-06 19:35:49 +0200

    Move some files to subdirectories

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
 import { UserController } from './controllers/user.controller';
+import { RideController } from './controllers/ride.controller';
+import { RouteController } from './controllers/route.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController, RideController, RouteController],
   providers: [AppService],
 })
 export class AppModule {}
