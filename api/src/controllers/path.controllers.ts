38c080cf5ea13c4d483319f7e48634c5ef41f055
Artur Siepietowski <artursiepietowski@gmail.com>
2019-04-07 01:00:17 +0200

    Fix

--- api/src/controllers/path.controllers.ts
+++ api/src/controllers/path.controllers.ts
@@ -3,28 +3,28 @@ import { IPath } from '../../../models/iPath';
 
 @Controller('Paths')
 export class PathController {
-    @Post()
-    create(@Body() path: IPath) {
-        return 'This action adds a new path';
-    }
+  @Post()
+  create(@Body() path: IPath) {
+    return 'This action adds a new path';
+  }
 
-    @Get()
-    findAll() {
-        return `This action returns all path items`;
-    }
+  @Get()
+  findAll() {
+    return `This action returns all path items`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `This action returns a #${id} path`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} path`;
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() path: IPath) {
-        return `This action updates a #${id} path`;
-    }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() path: IPath) {
+    return `This action updates a #${id} path`;
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `This action removes a #${id} path`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} path`;
+  }
 }
--- api/src/controllers/preference.controllers.ts
+++ api/src/controllers/preference.controllers.ts
@@ -3,28 +3,28 @@ import { IPreference } from '../../../models/iPreference';
 
 @Controller('preferences')
 export class PreferenceController {
-    @Post()
-    create(@Body() preference: IPreference) {
-        return 'This action adds a new preference';
-    }
+  @Post()
+  create(@Body() preference: IPreference) {
+    return 'This action adds a new preference';
+  }
 
-    @Get()
-    findAll() {
-        return `This action returns all preference items`;
-    }
+  @Get()
+  findAll() {
+    return `This action returns all preference items`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `This action returns a #${id} preference`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} preference`;
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() preference: IPreference) {
-        return `This action updates a #${id} preference`;
-    }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() preference: IPreference) {
+    return `This action updates a #${id} preference`;
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `This action removes a #${id} preference`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} preference`;
+  }
 }
