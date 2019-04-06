94f21a155432249f803d82dbd1d5a7cf25272725
Maciej Hełmecki <mhelmecki@gmail.com>
2019-04-06 23:38:42 +0200

    Control commit

--- api/src/controllers/path.controllers.ts
+++ api/src/controllers/path.controllers.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { Path } from '../../../models/path';
 
 @Controller('Paths')
