909a86e9470aeb14fcf03649d7ea28338d404b23
Maciej Hełmecki <mhelmecki@gmail.com>
2019-04-06 19:51:01 +0200

    Update app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './services/app.service';
 import { UserController } from './controllers/user.controller';
 import { RideController } from './controllers/ride.controller';
 import { RouteController } from './controllers/route.controller';
+import { PathController } from './controllers/path.controllers';
+import { PreferenceController } from './controllers/preference.controllers';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, RideController, RouteController],
+  controllers: [
+    AppController,
+    UserController,
+    RideController,
+    RouteController,
+    PathController, 
+    PreferenceController
+  ],
   providers: [AppService],
 })
 export class AppModule {}
