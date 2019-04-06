8a9d69f9dab79390666dfb2b37b1123319223922
Maciej Hełmecki <mhelmecki@gmail.com>
2019-04-07 00:26:02 +0200

    Control commit

---
+++ api/src/services/name.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../../../models/user';
+
+@Injectable()
+export class NameService {
+    get
+}
+
+
+export interface RouteDistance {
+    user: User;
+    distance: number;
+}
+  
