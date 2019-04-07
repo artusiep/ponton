055a381f0460c37ea50503d4adffd99ac3ecef36
admin <admin@DESKTOP-1S85TRJ>
2019-04-07 02:16:14 +0200

    merging maps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import './App.css';
 import { RouteWizard } from './routeWizard/RouteWizard';
-import { MapPicker } from './components/MapPicker';
 
 class App extends React.Component {
-  handleOnChange = () => {
-  }
 
   render() {
-    return <MapPicker height={500} width={500} onChange={ this.handleOnChange } />;
+    return <RouteWizard />;
   }
 }
 
